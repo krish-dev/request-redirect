39f4d8e37179f4ea9e5cca17d4900c22c789dcb7
Krishnendu Sekhar Das <krish@Krishnendus-Mac-mini.local>
2022-12-30 12:20:56 +0530

    add react-hook-form for form handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
-import { useState } from "react";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import { DataGrid } from "@mui/x-data-grid";
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Switch from '@mui/material/Switch'
+import { DataGrid } from '@mui/x-data-grid'
 
-import NoRowsOverlay from "./components/NoData";
-import AddRule from "./components/AddRule";
+import NoRowsOverlay from './components/NoData'
+import AddRule from './components/AddRule'
+import { useLocalStorage } from './hooks/storage'
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false)
+  const [rules, setRules] = useLocalStorage('rules', [])
 
   const handleChange = (event) => {
     // chrome.runtime.sendMessage({type: 'request_proxy:control', data: event.target.checked});
-    setChecked(event.target.checked);
-  };
+    setChecked(event.target.checked)
+  }
+
+  const delCell = (params) => {
+    return (
+      <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+    )
+  }
+
+  const activeControlCell = (params) => {
+    const handleChange = (event) => {
+      console.log(params.row)
+    }
+    return <Switch checked={params.row.isActive} onChange={handleChange} />
+  }
 
   const columns = [
-    { field: "from", headerName: "From", width: 350, editable: true },
-    { field: "to", headerName: "To", width: 350, editable: true },
-  ];
+    { field: 'isActive', headerName: 'State', renderCell: activeControlCell },
+    { field: 'from', headerName: 'From', width: 280, editable: true },
+    { field: 'to', headerName: 'To', width: 280, editable: true },
+    { field: 'delete', headerName: 'Action', renderCell: delCell },
+  ]
 
-  const rows = [];
-
-  const handleAddedRule = ({from, to}) => {
-    console.log(from, to);
+  const handleAddedRule = ({ from, to }) => {
+    const existingRules = [...rules]
+    existingRules.push({ id: existingRules.length + 1, from, to, isActive: true })
+    setRules(existingRules)
   }
 
   return (
@@ -31,15 +53,14 @@ function App() {
       <Box sx={{ flexGrow: 1 }} p={2}>
         <Grid container spacing={2}>
           <Grid item xs={12} container justifyContent="flex-end">
-            <AddRule onRuleAdded={handleAddedRule}/>
+            <AddRule onRuleAdded={handleAddedRule} />
           </Grid>
           <Grid item xs={12}>
             <Paper elevation={3}>
-              <Box sx={{ height: 400, width: "100%" }}>
+              <Box sx={{ height: 400, width: '100%' }}>
                 <DataGrid
-                  rows={rows}
+                  rows={rules}
                   columns={columns}
-                  checkboxSelection
                   disableSelectionOnClick
                   hideFooter={true}
                   experimentalFeatures={{ newEditingApi: true }}
@@ -53,7 +74,7 @@ function App() {
         </Grid>
       </Box>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/AddRule.jsx
+++ src/components/AddRule.jsx
@@ -1,76 +1,35 @@
-import { useState } from "react";
-import Box from "@mui/material/Box";
-import Backdrop from "@mui/material/Backdrop";
-import Modal from "@mui/material/Modal";
-import Grid from "@mui/material/Grid";
-import Fade from "@mui/material/Fade";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Backdrop from '@mui/material/Backdrop'
+import Modal from '@mui/material/Modal'
+import Grid from '@mui/material/Grid'
+import Fade from '@mui/material/Fade'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import { useForm, Controller } from 'react-hook-form'
 
-const AddRule = ({onRuleAdded}) => {
+const AddRule = ({ onRuleAdded }) => {
   const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
     width: 600,
-    bgcolor: "background.paper",
+    bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
-  };
-
-  const initialState = {
-    from: {
-      value: "",
-      error: false,
-      errorMessage: "You must enter a url",
-    },
-    to: {
-      value: "",
-      error: false,
-      errorMessage: "You must enter an url",
-    },
-  };
-  const [modalOpen, setModalOpen] = useState(false);
-  const [formValues, setFormValues] = useState(initialState);
-  const handleOpen = () => setModalOpen(true);
-  const handleClose = () => setModalOpen(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formFields = Object.keys(formValues);
-    let newFormValues = { ...formValues };
-    for (let index = 0; index < formFields.length; index++) {
-      const currentField = formFields[index];
-      const currentValue = formValues[currentField].value;
-      newFormValues = {
-        ...newFormValues,
-        [currentField]: {
-          ...newFormValues[currentField],
-          error: currentValue === '' ? true: false,
-        },
-      };
-    }
-    setFormValues({...newFormValues});
-    console.log(formValues);
-    if(!formValues.from.error && !formValues.to.error) {
-        onRuleAdded({from: formValues.from.value, to: formValues.to.value});
-        // setFormValues({...initialState });
-        // setModalOpen(false);
-    }
-  };
-
-
-  const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormValues({
-      ...formValues,
-      [name]:{
-        ...formValues[name],
-        value
-      }
-    })
+  }
+  /** Modal  handle */
+  const [modalOpen, setModalOpen] = useState(false)
+  const handleOpen = () => setModalOpen(true)
+  const handleClose = () => setModalOpen(false)
+  /** Form handle */
+  const { handleSubmit, control, reset } = useForm()
+  const onSubmit = (data) => {
+    onRuleAdded(data)
+    reset({ from: '', to: '' })
+    handleClose()
   }
 
   return (
@@ -90,41 +49,51 @@ const AddRule = ({onRuleAdded}) => {
         }}
       >
         <Fade in={modalOpen}>
-          <Box sx={style} component="form" noValidate onSubmit={handleSubmit} onChange={handleChange}>
+          <Box sx={style} component="form" noValidate onSubmit={handleSubmit(onSubmit)}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <Typography
-                  id="transition-modal-title"
-                  variant="h6"
-                  component="h2"
-                >
+                <Typography id="transition-modal-title" variant="h6" component="h2">
                   Add redirect rules
                 </Typography>
               </Grid>
               <Grid item xs={12}>
-                <TextField
+                <Controller
                   name="from"
-                  required
-                  fullWidth
-                  value={formValues.from.value}
-                  error={formValues.from.error}
-                  size="small"
-                  label="From"
-                  helperText={
-                    formValues.from.error && formValues.from.errorMessage
-                  }
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: 'URL required' }}
+                  render={({ field: { onChange, value }, fieldState: { error } }) => (
+                    <TextField
+                      required
+                      fullWidth
+                      size="small"
+                      label="From"
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                    />
+                  )}
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
+                <Controller
                   name="to"
-                  required
-                  fullWidth
-                  value={formValues.to.value}
-                  error={formValues.to.error}
-                  size="small"
-                  label="To"
-                  helperText={formValues.to.error && formValues.to.errorMessage}
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: 'URL required' }}
+                  render={({ field: { onChange, value }, fieldState: { error } }) => (
+                    <TextField
+                      required
+                      fullWidth
+                      size="small"
+                      label="To"
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                    />
+                  )}
                 />
               </Grid>
               <Grid item xs={12} container justifyContent="flex-end">
@@ -137,7 +106,7 @@ const AddRule = ({onRuleAdded}) => {
         </Fade>
       </Modal>
     </>
-  );
-};
+  )
+}
 
-export default AddRule;
+export default AddRule
